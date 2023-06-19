0a08a1ffa4e74ccb744c151b3a3e4b66a344cf08
Daddyjohn63 <john@webandprosper.co.uk>
2023-06-19 19:38:09 +0100

    working but does not recall previous messages.

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -4,37 +4,61 @@ import { OpenAIEdgeStream } from "openai-edge-stream"; //calling openai
 export const config = {
   runtime: "edge",
 };
-
+//as it is an edge function, it only has access to the request object (req).
 export default async function handler(req) {
   //the req object represents any data that we send to our API endpoint.In this case it is the message/ question from the user.
   try {
     //this is the message that the user types in the text box.
     //console.log("HERE IS THE REQ TO THE API ENPOINT", req.body);
-    const { message } = await req.json();
+    const { chatId: chatIdFromParam, message } = await req.json();
+    let chatId = chatIdFromParam;
+
     const initialChatMessage = {
       role: "system",
       content:
         "Your name is DaddyDude. An incredibly intelligent and quick-thinking AI. You always reply with an enthusiastic and positive energy. You were created by Web and Prosper.Your response must be formatted with markdown",
     };
 
-    const response = await fetch(
-      //domain name required before api endpoint as we are targetting from a server component.
-      `${req.headers.get("origin")}/api/chat/createNewChat`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          cookie: req.headers.get("cookie"),
-        },
-        body: JSON.stringify({
-          message, //this is what is passed in above with 'const { message } = await req.json();'
-        }),
-      }
-    );
-    const json = await response.json();
-    //console.log("JSON CNC:", json);
-    const chatId = json._id;
+    let newChatId;
 
+    if (chatId) {
+      //add message to existing chat
+      const response = await fetch(
+        `${req.headers.get("origin")}/api/chat/addMessageToChat`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({
+            chatId,
+            role: "user",
+            content: message,
+          }),
+        }
+      );
+    } else {
+      //create new chat
+      const response = await fetch(
+        //domain name required before api endpoint as we are targetting from a server component.
+        `${req.headers.get("origin")}/api/chat/createNewChat`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({
+            message, //this is what is passed in above with 'const { message } = await req.json();'
+          }),
+        }
+      );
+      const json = await response.json();
+      //console.log("JSON CNC:", json);
+      chatId = json._id;
+      newChatId = json._id;
+    }
     //send message to openai
     const stream = await OpenAIEdgeStream(
       "https://api.openai.com/v1/chat/completions", //the url we want to hit.
@@ -57,7 +81,9 @@ export default async function handler(req) {
       },
       {
         onBeforeStream: ({ emit }) => {
-          emit(chatId, "newChatId"); //this is a new event name we have created to be sent before the content stream, this way we can target the start of the stream.
+          if (newChatId) {
+            emit(newChatId, "newChatId"); //this is a new event name we have created to be sent before the content stream, this way we can target the start of the stream.
+          }
         },
         //call endpoint to add the new chat to exisitng chats.
         onAfterStream: async ({ fullContent }) => {
--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,19 +1,46 @@
+import { getSession } from "@auth0/nextjs-auth0";
 import { ChatSidebar } from "components/ChatSidebar";
 import { Message } from "components/Message";
+import clientPromise from "lib/mongodb";
+import { ObjectId } from "mongodb";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { streamReader } from "openai-edge-stream";
 import { useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
 
-export default function ChatPage({ chatId }) {
+export default function ChatPage({ chatId, title, messages = [] }) {
+  console.log("props: ", title, messages);
   const [newChatId, setNewChatId] = useState(null);
   const [incomingMessage, setIncomingMessage] = useState(""); // stores the most recent incoming message from chat gpt
   const [messageText, setMessageText] = useState(""); //stores the current text that the user has typed into the chat input.
   const [newChatMessages, setNewChatMessages] = useState([]); //stores the list of chat messages that have been sent so far by the user? in an array.
   const [generatingResponse, setGeneratingResponse] = useState(false);
+  const [fullMessage, setFullMessage] = useState("");
   const router = useRouter();
 
+  //when our route changes
+  useEffect(() => {
+    setNewChatMessages([]);
+    setNewChatId(null);
+  }, [chatId]);
+
+  //save the newly streamed message to new chat messages.
+  useEffect(() => {
+    if (!generatingResponse && fullMessage) {
+      setNewChatMessages((prev) => [
+        ...prev,
+        {
+          _id: uuid(),
+          role: "assistant",
+          content: fullMessage,
+        },
+      ]);
+      setFullMessage("");
+    }
+  }, [generatingResponse, fullMessage]);
+
+  //if we have created a new chat
   useEffect(() => {
     if (!generatingResponse && newChatId) {
       setNewChatId(null);
@@ -47,7 +74,7 @@ export default function ChatPage({ chatId }) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ message: messageText }), //what we pass here needs to match exactly with what we specified in our destructuring in sendMessage.js which was const { message } = await req.json(); This is the data we will send to our endpoint and the value is our state value.
+      body: JSON.stringify({ chatId, message: messageText }), //what we pass here needs to match exactly with what we specified in our destructuring in sendMessage.js which was const { message } = await req.json(); This is the data we will send to our endpoint and the value is our state value. We also send the chatId.
     });
     //get the reader
     const data = response.body;
@@ -56,6 +83,7 @@ export default function ChatPage({ chatId }) {
     }
     //read the data coming back from chatGPT and pass it into state.
     const reader = data.getReader();
+    let content = "";
     //below function comes from openai package.
     await streamReader(reader, async (message) => {
       console.log("MESSAGE: ", message);
@@ -63,11 +91,15 @@ export default function ChatPage({ chatId }) {
         setNewChatId(message.content);
       } else {
         setIncomingMessage((s) => `${s}${message.content}`); //add the new content to the previous state and return new state.
+        content = content + message.content;
       }
     });
-
+    setFullMessage(content);
+    setIncomingMessage("");
     setGeneratingResponse(false);
   };
+
+  const allMessages = [...messages, ...newChatMessages];
 
   return (
     <>
@@ -78,7 +110,7 @@ export default function ChatPage({ chatId }) {
         <ChatSidebar chatId={chatId} />
         <div className="flex flex-col overflow-hidden bg-gray-700">
           <div className="flex-1 overflow-auto text-white">
-            {newChatMessages.map((message) => (
+            {allMessages.map((message) => (
               <Message
                 key={message._id} //need the id as we are rendering from an array.we dont need to pass this component.
                 role={message.role}
@@ -112,10 +144,27 @@ export default function ChatPage({ chatId }) {
 }
 
 export const getServerSideProps = async (ctx) => {
-  const chatId = ctx.params?.chatId?.[0] || null; //the prop chatId needs to eb the same name we gave to the file [[...chatId]].js. Have to use null as we cannot pass undefined.
+  const chatId = ctx.params?.chatId?.[0] || null; //the prop chatId needs to be the same name we gave to the file [[...chatId]].js. Have to use null as we cannot pass undefined.
+  if (chatId) {
+    const { user } = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("JohnPaulChatGPT");
+    const chat = await db.collection("chats").findOne({
+      userId: user.sub,
+      _id: new ObjectId(chatId),
+    });
+    return {
+      props: {
+        chatId,
+        title: chat.title,
+        messages: chat.messages.map((message) => ({
+          ...message,
+          _id: uuid(),
+        })),
+      },
+    };
+  }
   return {
-    props: {
-      chatId,
-    },
+    props: {},
   };
 };
